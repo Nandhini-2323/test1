fc4ab09663f979a84f37eedd3f67c53e8c4f0e10
Nandhini-2323 <nandhini.ace@gmail.com>
2024-12-07 22:29:02 +0530

    waterbottle properties added

---
+++ test33/src/waterBottle/waterBottleProperties.java
@@ -0,0 +1,20 @@
+package waterBottle;
+
+public class waterBottleProperties {
+	
+	
+	
+	public static void main(String[] args) {
+		int capacity = 1000;
+		String colour = "blue";
+		int lenght = 30;
+		float radius = 2.5f;
+		
+		
+	System.out.println("The capacity of water bottle is " +capacity);
+	System.out.println("The colour of the bottle is " + colour);
+	System.out.println("The lenght of the bottle is" + lenght);
+	System.out.println("The radius of the water bottle is " + radius);
+
+}
+}
